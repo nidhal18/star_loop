37b8dcd6061b54243cd1bac68871628fff6e5021
Nidhal <alinidhal31@gmail.com>
2024-12-04 14:09:50 +0530

    first commit

---
+++ bin/star_loop.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    // Print a star without a new line
+    stdout.writeln('*');
+  }
+  // Move to the next line after printing the stars
+  
+}
